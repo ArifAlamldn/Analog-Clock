c673db673437d10b9dfa2f7d1160806eb9ec798e
arif alam <arifalamldn@gmail.com>
2024-12-21 21:45:24 +0530

    Updade Code

--- index.js
+++ index.js
@@ -7,15 +7,14 @@ function run() {
     let hours = date.getHours();
     let minute = date.getMinutes();
     let second = date.getSeconds();
-    
-    let hRotation = 30 * hours + minute / 2; // 30° per hour + 0.5° per minute
-    let mRotation = 6 * minute;             // 6° per minute
-    let sRotation = 6 * second;             // 6° per second
 
+    let hRotation = 30 * hours + minute / 2;
+    let mRotation = 6 * minute;             
+    let sRotation = 6 * second;            
     hr.style.transform = `rotate(${hRotation}deg)`;
     min.style.transform = `rotate(${mRotation}deg)`;
     sec.style.transform = `rotate(${sRotation}deg)`;
 }
 
 setInterval(run, 1000);
-run(); // Call run() once to initialize the clock immediately
+run(); 

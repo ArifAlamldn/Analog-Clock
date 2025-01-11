4d6bc066e2688548992df6e33d02e37db82cf8bd
arif alam <arifalamldn@gmail.com>
2025-01-11 23:32:15 +0530

    Code Update

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 let hr = document.querySelector(".hand.hours");
+
 let min = document.querySelector(".hand.minute");
 let sec = document.querySelector(".hand.second");
 
 function run() {
 
     
-    
+
     let date = new Date();
     let hours = date.getHours();
     let minute = date.getMinutes();

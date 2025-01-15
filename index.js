b4268e8813ee5d689466488626a01a6c7beb9d2b
arif alam <arifalamldn@gmail.com>
2025-01-15 21:23:54 +0530

    code

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
 let hr = document.querySelector(".hand.hours");
 let min = document.querySelector(".hand.minute");
-
-
 let sec = document.querySelector(".hand.second");
 
 function run() {
-
-    
-
     let date = new Date();
     let hours = date.getHours();
     let minute = date.getMinutes();
     let second = date.getSeconds();
-
     let hRotation = 30 * hours + minute / 2;
     let mRotation = 6 * minute;             
     let sRotation = 6 * second;      
           
+    
     hr.style.transform = `rotate(${hRotation}deg)`;
     min.style.transform = `rotate(${mRotation}deg)`;
     sec.style.transform = `rotate(${sRotation}deg)`;
